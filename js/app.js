34ab5fde1efbda67e661a8a436e9a87bc4377d01
Brenda Rychter <brendarychter@gmail.com>
2016-06-05 00:14:42 -0300

    geo

--- js/app.js
+++ js/app.js
@@ -11,9 +11,30 @@ $(document).on("mobileinit", function () {
 });
 */
 
-window.addEventListener("batterystatus", onBatteryStatus, false);
-
+document.addEventListener("deviceready", onBatteryStatus, false);
 function onBatteryStatus(info) {
+	console.log("hola")
     // Handle the online event
     alert("Level: " + info.level + " isPlugged: " + info.isPlugged);
 }
+/*
+cordova.plugins.barcodeScanner.encode(cordova.plugins.barcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com", function(success) {
+    alert("encode success: " + success);
+  }, function(fail) {
+    alert("encoding failed: " + fail);
+  }
+);
+
+*/
+function Generate(){    
+     var qrcode = new QRCode("qrcode");    
+     var TBCode = document.getElementById("text");    
+        
+    if (!TBCode.value) {    
+        alert("Input a text");    
+        TBCode.focus();    
+        return;    
+    
+    }
+}
+    qrcode.makeCode(TBCode.value);  
